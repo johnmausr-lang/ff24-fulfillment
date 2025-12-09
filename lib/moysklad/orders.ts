10e9ca229c46199ffcf76aefd74165146c7e665a
johnmausr-lang <johnmausr@gmail.com>
2025-12-09 17:02:43 +0500

    Update orders.ts

--- lib/moysklad/orders.ts
+++ lib/moysklad/orders.ts
@@ -1,29 +1,14 @@
-// lib/moysklad/orders.ts
-
 import { MoyskladClient } from "./client";
 
 export class OrdersService {
-  constructor(private client: MoyskladClient) {}
+  client: MoyskladClient;
 
-  /**
-   * Получение списка заказов
-   */
-  async list(limit = 50) {
-    const res = await this.client.getOrders(limit);
+  constructor(client: MoyskladClient) {
+    this.client = client;
+  }
+
+  async list() {
+    const res = await this.client.getOrders();
     return res?.rows ?? [];
-  }
-
-  /**
-   * Получение одного заказа
-   */
-  async getById(id: string) {
-    return this.client.getOrder(id);
-  }
-
-  /**
-   * Создание заказа
-   */
-  async create(data: any) {
-    return this.client.createOrder(data);
   }
 }
