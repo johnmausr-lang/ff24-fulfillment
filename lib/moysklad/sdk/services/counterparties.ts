7fac8e589685e8bef3f77eb01db5f76e44bd4496
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 15:44:09 +0500

    Update counterparties.ts

--- lib/moysklad/sdk/services/counterparties.ts
+++ lib/moysklad/sdk/services/counterparties.ts
@@ -1,22 +1,15 @@
-import { MSClient } from "../../client";
-import { MSCounterparty } from "../../types";
+import { MSClient } from "../client";
+import { MSCounterparty } from "../types";
 
 export class CounterpartyService {
   constructor(private client: MSClient) {}
 
-  async list() {
-    const res = await this.client.get("/entity/counterparty", { limit: 200 });
-    return res.rows as MSCounterparty[];
+  async list(params: { limit?: number; search?: string } = {}): Promise<MSCounterparty[]> {
+    const res = await this.client.get("/entity/counterparty", params);
+    return res.rows ?? [];
   }
 
-  async getById(id: string) {
+  async getById(id: string): Promise<MSCounterparty> {
     return await this.client.get(`/entity/counterparty/${id}`);
-  }
-
-  async findByEmail(email: string) {
-    const res = await this.client.get("/entity/counterparty", {
-      filter: `email=${email}`,
-    });
-    return res.rows?.[0] || null;
   }
 }
