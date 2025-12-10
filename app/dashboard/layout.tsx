b6fe706a1f5ed9809357da7b707d09a90a2150d3
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 12:56:07 +0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,21 @@
-"use client";
-
 import Sidebar from "@/components/dashboard/Sidebar";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
+import { ReactNode } from "react";
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <div className="flex min-h-screen bg-[#0F0F0F] text-white">
       <Sidebar />
 
-      <div className="flex flex-col flex-1">
+      <div className="flex-1 flex flex-col">
         <DashboardHeader />
 
         <main className="p-10">
-          <div className="max-w-7xl mx-auto">{children}</div>
+          {children}
         </main>
       </div>
     </div>
