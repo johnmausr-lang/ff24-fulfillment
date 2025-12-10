3967900f429a124e5f2df2e4f0b72b6283d9d6f8
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 15:43:10 +0500

    Update inventory.ts

--- lib/moysklad/sdk/services/inventory.ts
+++ lib/moysklad/sdk/services/inventory.ts
@@ -1,11 +1,11 @@
-import { MSClient } from "../../client";
-import { MSInventoryRow } from "../../types";
+import { MSClient } from "../client";
+import { MSInventoryRow } from "../types";
 
 export class InventoryService {
   constructor(private client: MSClient) {}
 
-  async list(limit: number = 200) {
-    const res = await this.client.get("/report/stock/all", { limit });
-    return res.rows as MSInventoryRow[];
+  async list(params: { limit?: number } = {}): Promise<MSInventoryRow[]> {
+    const res = await this.client.get("/report/stock/all", params);
+    return res.rows ?? [];
   }
 }
