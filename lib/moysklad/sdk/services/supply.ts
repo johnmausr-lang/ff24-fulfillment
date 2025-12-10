98f793fc64dfead3b42e12d6449680f0a4e7b25f
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 14:32:13 +0500

    Update supply.ts

--- lib/moysklad/sdk/services/supply.ts
+++ lib/moysklad/sdk/services/supply.ts
@@ -1,29 +1,10 @@
 import { MSClient } from "../../client";
-
-export interface MSSupply {
-  id: string;
-  name: string;
-  description?: string;
-  created?: string;
-}
+import { MSSupply } from "../../types";
 
 export class SupplyService {
-  private client: MSClient;
+  constructor(private client: MSClient) {}
 
-  constructor(client: MSClient) {
-    this.client = client;
-  }
-
-  async list(limit: number = 50): Promise<MSSupply[]> {
-    const res = await this.client.get("/entity/supply", { limit });
-    return res.rows ?? [];
-  }
-
-  async getById(id: string): Promise<MSSupply> {
-    return await this.client.get(`/entity/supply/${id}`);
-  }
-
-  async create(payload: any): Promise<MSSupply> {
-    return await this.client.post(`/entity/supply`, payload);
+  async create(body: any): Promise<MSSupply> {
+    return await this.client.post("/entity/supply", body);
   }
 }
