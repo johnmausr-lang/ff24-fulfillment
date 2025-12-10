646cb8c23c5f8766ca8d2ad802003ed9a9cf24d4
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 11:41:58 +0500

    Update orders.ts

--- lib/moysklad/sdk/services/orders.ts
+++ lib/moysklad/sdk/services/orders.ts
@@ -1,17 +1,25 @@
 import { MSClient } from "../../client";
 
+export interface MSOrder {
+  id: string;
+  name: string;
+  description?: string;
+  created?: string;
+}
+
 export class OrdersService {
-  client: MSClient;
+  private client: MSClient;
 
   constructor(client: MSClient) {
     this.client = client;
   }
 
-  async list(limit: number = 50) {
-    return await this.client.get("/entity/customerorder", { limit });
+  async list(limit: number = 50): Promise<MSOrder[]> {
+    const res = await this.client.get("/entity/customerorder", { limit });
+    return res.rows ?? [];
   }
 
-  async getById(id: string) {
+  async getById(id: string): Promise<MSOrder> {
     return await this.client.get(`/entity/customerorder/${id}`);
   }
 }
