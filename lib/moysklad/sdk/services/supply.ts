72fdc5cac99a1d62465d11760d3f7c4ab5945948
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 11:16:35 +0500

    Update supply.ts

--- lib/moysklad/sdk/services/supply.ts
+++ lib/moysklad/sdk/services/supply.ts
@@ -1,25 +1,30 @@
+import { MSClient } from "../../client";
+
 export class SupplyService {
-  constructor(private client: any) {}
+  client: MSClient;
+
+  constructor(client: MSClient) {
+    this.client = client;
+  }
 
   /**
    * Создать документ "Приёмка"
-   * https://dev.moysklad.ru/doc/api/remap/1.2/documents/#dokhody-priemka
    */
   async create(payload: any) {
     return await this.client.post("/entity/supply", payload);
   }
 
   /**
-   * Получить документ Приёмки по ID
+   * Получить документ по ID
    */
   async getById(id: string) {
     return await this.client.get(`/entity/supply/${id}`);
   }
 
   /**
-   * Список приходных накладных
+   * Получить список приёмок
    */
-  async list(limit = 50) {
+  async list(limit: number = 50) {
     return await this.client.get("/entity/supply", { limit });
   }
 }
