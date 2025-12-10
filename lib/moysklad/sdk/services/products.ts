15158291d8c990d04cd67494444ed77f3a332c27
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 15:43:55 +0500

    Update products.ts

--- lib/moysklad/sdk/services/products.ts
+++ lib/moysklad/sdk/services/products.ts
@@ -1,15 +1,15 @@
-import { MSClient } from "../../client";
-import { MSProduct } from "../../types";
+import { MSClient } from "../client";
+import { MSProduct } from "../types";
 
 export class ProductsService {
   constructor(private client: MSClient) {}
 
-  async list(limit: number = 200) {
-    const res = await this.client.get("/entity/product", { limit });
-    return res.rows as MSProduct[];
+  async list(params: { limit?: number; search?: string } = {}): Promise<MSProduct[]> {
+    const res = await this.client.get("/entity/product", params);
+    return res.rows ?? [];
   }
 
-  async getById(id: string) {
+  async getById(id: string): Promise<MSProduct> {
     return await this.client.get(`/entity/product/${id}`);
   }
 }
