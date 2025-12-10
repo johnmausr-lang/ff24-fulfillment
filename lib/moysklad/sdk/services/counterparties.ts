73be24b66f06a6d21d737e1c17b760ac339c3af8
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 11:16:08 +0500

    Update counterparties.ts

--- lib/moysklad/sdk/services/counterparties.ts
+++ lib/moysklad/sdk/services/counterparties.ts
@@ -1,12 +1,24 @@
-export class CounterpartyService {
-  constructor(private client: any) {}
+import { MSClient } from "../../client";
 
+export class CounterpartyService {
+  client: MSClient;
+
+  constructor(client: MSClient) {
+    this.client = client;
+  }
+
+  /**
+   * Найти контрагента по E-mail
+   */
   async findByEmail(email: string) {
     return await this.client.get("/entity/counterparty", {
-      filter: `email=${email}`
+      filter: `email=${email}`,
     });
   }
 
+  /**
+   * Получить контрагента по ID
+   */
   async getById(id: string) {
     return await this.client.get(`/entity/counterparty/${id}`);
   }
