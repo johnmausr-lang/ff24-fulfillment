7a46837490e10ca95a80fd0d36f7cabea4b5826c
johnmausr-lang <johnmausr@gmail.com>
2025-12-10 10:18:34 +0500

    Update route.ts

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -5,15 +5,21 @@ export async function GET() {
   try {
     const ms = createMoyskladSDK();
 
-    const orders = await ms.orders.list(100);
+    const orders = await ms.orders.list(200);
 
     return NextResponse.json({
       success: true,
-      data: orders?.rows ?? [],
+      data: orders,
     });
+
   } catch (err: any) {
+    console.error("ORDERS API ERROR:", err);
+
     return NextResponse.json(
-      { success: false, error: err.message },
+      {
+        success: false,
+        error: err?.message ?? "Unexpected server error",
+      },
       { status: 500 }
     );
   }
