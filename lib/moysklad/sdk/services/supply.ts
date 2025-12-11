2af5bfd945ad99da87a14727592b4fc726dc99b2
johnmausr-lang <johnmausr@gmail.com>
2025-12-11 09:07:13 +0500

    Update supply.ts

--- lib/moysklad/sdk/services/supply.ts
+++ lib/moysklad/sdk/services/supply.ts
@@ -8,16 +8,10 @@ export class SupplyService {
     this.client = client;
   }
 
-  /**
-   * Создать документ "Приёмка"
-   */
   async create(payload: any): Promise<MSSupply> {
     return this.client.post("/entity/supply", payload);
   }
 
-  /**
-   * Получить документ приёмки по ID
-   */
   async getById(id: string): Promise<MSSupply | null> {
     return this.client.get(`/entity/supply/${id}`);
   }
