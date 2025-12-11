607487d227468d8f6ae7355901ba082393a18a17
johnmausr-lang <johnmausr@gmail.com>
2025-12-11 09:01:06 +0500

    Update supply.ts

--- lib/moysklad/sdk/services/supply.ts
+++ lib/moysklad/sdk/services/supply.ts
@@ -1,10 +1,24 @@
-import { MSClient } from "../client";
-import { MSSupply } from "../types";
+import { MSClient } from "../../client";
+import { MSSupply } from "../../types";
 
 export class SupplyService {
-  constructor(private client: MSClient) {}
+  client: MSClient;
 
+  constructor(client: MSClient) {
+    this.client = client;
+  }
+
+  /**
+   * Создать документ "Приёмка"
+   */
   async create(payload: any): Promise<MSSupply> {
-    return await this.client.post("/entity/supply", payload);
+    return this.client.post("/entity/supply", payload);
+  }
+
+  /**
+   * Получить документ приёмки по ID
+   */
+  async getById(id: string): Promise<MSSupply | null> {
+    return this.client.get(`/entity/supply/${id}`);
   }
 }
