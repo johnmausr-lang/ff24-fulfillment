7a2437abf8d68e2d95c568a76dcf28b22f51c65b
johnmausr-lang <johnmausr@gmail.com>
2025-12-15 16:34:36 +0500

    Update Portal.tsx

--- components/three/Portal.tsx
+++ components/three/Portal.tsx
@@ -1,29 +1,36 @@
 "use client";
 
-import { MeshProps } from "@react-three/fiber";
-
-export default function Portal({
-  z = 0,
-}: {
-  z: number;
-}) {
+export default function Portal({ z }: { z: number }) {
   return (
-    <group position={[0, 2.5, z]}>
-      {/* Основная рама */}
+    <group position={[0, 2.6, z]}>
+      {/* ВНУТРЕННЯЯ ТЬМА */}
       <mesh>
-        <boxGeometry args={[6, 4, 0.3]} />
-        <meshStandardMaterial color="#0a0a0a" />
+        <boxGeometry args={[6, 4, 0.6]} />
+        <meshStandardMaterial
+          color="#060606"
+          roughness={0.6}
+          metalness={0.1}
+        />
       </mesh>
 
-      {/* Неоновая окантовка */}
-      <mesh position={[0, 0, 0.16]}>
+      {/* НЕОНОВАЯ РАМКА */}
+      <mesh position={[0, 0, 0.36]}>
         <boxGeometry args={[6.4, 4.4, 0.05]} />
         <meshStandardMaterial
           color="#28064F"
           emissive="#8A2BE2"
-          emissiveIntensity={2.8}
+          emissiveIntensity={3}
+          toneMapped={false}
         />
       </mesh>
+
+      {/* ВНУТРЕННИЙ СВЕТ */}
+      <pointLight
+        position={[0, 0, 0.8]}
+        intensity={1.5}
+        distance={6}
+        color="#8A2BE2"
+      />
     </group>
   );
 }
