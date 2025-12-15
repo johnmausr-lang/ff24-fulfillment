a64ec3c7e38410d401485a51a9a35a3daa27b3b5
johnmausr-lang <johnmausr@gmail.com>
2025-12-15 16:34:17 +0500

    Update WarehouseScene.tsx

--- components/three/WarehouseScene.tsx
+++ components/three/WarehouseScene.tsx
@@ -1,34 +1,47 @@
 "use client";
 
-import { Environment, ContactShadows } from "@react-three/drei";
+import { Environment, ContactShadows, Fog } from "@react-three/drei";
 import Portal from "./Portal";
 
 export default function WarehouseScene() {
   return (
     <>
-      {/* Свет */}
-      <ambientLight intensity={0.4} />
+      {/* ТУМАН — КЛЮЧЕВО ДЛЯ DEPTH */}
+      <Fog
+        attach="fog"
+        args={["#050505", 8, 32]}
+      />
+
+      {/* СВЕТ */}
+      <ambientLight intensity={0.35} />
+
       <directionalLight
-        position={[5, 10, 5]}
-        intensity={1.4}
+        position={[6, 10, 4]}
+        intensity={1.2}
         color="#ffffff"
       />
 
-      {/* Пол + тени */}
+      <pointLight
+        position={[-6, 4, -8]}
+        intensity={2}
+        color="#8A2BE2"
+      />
+
+      {/* ПОЛ + КОНТАКТНЫЕ ТЕНИ */}
       <ContactShadows
         position={[0, 0, 0]}
         opacity={0.45}
         scale={40}
-        blur={2}
-        far={20}
+        blur={2.5}
+        far={30}
       />
 
-      {/* Порталы */}
+      {/* ПОРТАЛЫ (СЛОИ ГЛУБИНЫ) */}
       <Portal z={-4} />
       <Portal z={-12} />
-      <Portal z={-20} />
+      <Portal z={-22} />
 
-      {/* Окружение */}
+      {/* ОКРУЖЕНИЕ */}
       <Environment preset="warehouse" />
     </>
   );
