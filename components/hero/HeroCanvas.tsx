3b43658ada330688f9c3ed030a9a2c2f4b4c8f5f
johnmausr-lang <johnmausr@gmail.com>
2025-12-15 17:14:33 +0500

    Update HeroCanvas.tsx

--- components/hero/HeroCanvas.tsx
+++ components/hero/HeroCanvas.tsx
@@ -2,29 +2,43 @@
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { Environment, ContactShadows } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import Scene from "./Scene";
 import CameraRig from "./CameraRig";
 
 export default function HeroCanvas() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        inset: 0,
-        zIndex: -1,
-      }}
+    <Canvas
+      shadows
+      dpr={[1, 1.5]}
+      camera={{ position: [0, 1.8, 6], fov: 45 }}
+      style={{ height: "100vh", width: "100vw" }}
     >
-      <Canvas
-        camera={{ position: [0, 2, 10], fov: 50 }}
-        dpr={[1, 2]}
-      >
-        <color attach="background" args={["#050505"]} />
+      <color attach="background" args={["#050505"]} />
 
-        <Suspense fallback={null}>
-          <Scene />
-          <CameraRig />
-        </Suspense>
-      </Canvas>
-    </div>
+      <Suspense fallback={null}>
+        <CameraRig />
+        <Scene />
+
+        <ContactShadows
+          position={[0, -1.4, 0]}
+          opacity={0.6}
+          scale={10}
+          blur={2.5}
+          far={5}
+        />
+
+        <Environment preset="warehouse" />
+      </Suspense>
+
+      <EffectComposer>
+        <Bloom
+          intensity={0.6}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.9}
+        />
+      </EffectComposer>
+    </Canvas>
   );
 }
