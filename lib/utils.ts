f68ea33f310cc0ce730d0a24607ae7764f388c52
johnmausr-lang <johnmausr@gmail.com>
2025-12-16 14:26:19 +0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-// src/lib/utils.ts
+// lib/utils.ts
 import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"   // ← ИМЕННО "tailwind-merge", а не "tw-merge"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
