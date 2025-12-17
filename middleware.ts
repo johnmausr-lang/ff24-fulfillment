1600f1564e92bc578a8c6a98afbf6ab6ba352ce5
johnmausr-lang <johnmausr@gmail.com>
2025-12-17 16:22:27 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // Временно разрешаем всем доступ ко всему, чтобы проверить работу Дашборда
+  const token = request.cookies.get('token')?.value;
+  const isAuthPage = request.nextUrl.pathname.startsWith('/login');
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard/:path*', '/login'],
 };
